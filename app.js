db07d8fdc2499be1d5180b32d5206ff2308260b5
ElDuderino96 <54913191+ElDuderino96@users.noreply.github.com>
2019-10-17 09:57:06 -0700

    2nd commit

--- app.js
+++ app.js
@@ -1,16 +1,71 @@
-const balloon = document.getElementById('balloon')
+const balloon = document.querySelector('b');
+balloon.style.fontSize = '30px';
 
-balloon.addEventListener("keyup", "I", balloon, function () => {
-    let balloonSize = parseInt(balloon.style.width);
-    let newSize = balloonSize + 10;
-    balloon.style.width = newSize.toString() + 'px'
+function inflate() {
+    let fontSizeStr = window.getComputedStyle(balloon).getPropertyValue('font-size')
+    let numStr = fontSizeStr.substr(0, fontSizeStr.length - 2)
+    let size = parseInt(numStr, 10)
+
+    if size > 60 {
+        boom (balloon)
+        document.removeEventListener('keyup', handlKeyup)
+    } else {
+        let newFontSize = (size + 10) + 'px'
+        balloon.style.fontSIze = newFontSize;
+    }
 }
-)
 
+function deflate() {
+    let fontSizeStr = window.getComputedStyle(balloon).getPropertyValue('font-size')
+    let numStr = fontSIzeStr.substr(0, fontSizeStr.length - 2)
+    let size = parseInt(numStr, 10)
 
-balloon.addEventListener("keydown", "D", balloon, function () => {
-    let balloonSize = parseInt(balloon.style.width);
-    let newSize = balloonSize - 10;
-    balloon.style.width = newSize.toString() + 'px'
+    if (size <= 0) {
+        balloon.replaceWith('Done')
+        var sheet = document.createElement('style')
+        sheet.innerHTML = 'div {font-size: 50px; text-align: center; margin-right: auto; margin-left: auto'
+        document.body.appendChild(sheet);
+        document.removeEventListener('keyup', handlKeyup)
+    } else {
+        let newFontSize = (size - 10) + 'px'
+        balloon.style.fontSize = newFontSize;
+    }
 }
-)
+
+function boom(balloon) {
+    balloon.firstChild.nodevalue = ''
+}
+
+function handlKeyup(event) {
+    if (event.code == 'KeyI') {
+        inflate()
+    }
+
+    if (event.code == 'KeyD') {
+        deflate()
+    }
+}
+
+document.addEventListener('keyup', handlKeyup)
+
+
+
+
+
+
+// const balloon = document.getElementById('balloon')
+
+// balloon.addEventListener("keyup", "I", balloon, function () => {
+//     let balloonSize = parseInt(balloon.style.width);
+//     let newSize = balloonSize + 10;
+//     balloon.style.width = newSize.toString() + 'px'
+// }
+// )
+
+
+// balloon.addEventListener("keydown", "D", balloon, function () => {
+//     let balloonSize = parseInt(balloon.style.width);
+//     let newSize = balloonSize - 10;
+//     balloon.style.width = newSize.toString() + 'px'
+// }
+// )
